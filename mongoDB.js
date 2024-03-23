cdca19a7c84c9dd8268cb3b72e654d1d8bc9139f
takaexe <164596411+takaexe@users.noreply.github.com>
2024-03-23 03:02:03 -0400

    mongoDB.js

--- mongoDB.js
+++ mongoDB.js
@@ -33,6 +33,11 @@ const playlist = Schema({
 module.exports = {
   musicbot: model("musicbot", musicbot),
   playlist: model("playlist", playlist)
+  mongoose.set('strictQuery', false);
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+ useUnifiedTopology: true,
+});
 };
 /*
 
