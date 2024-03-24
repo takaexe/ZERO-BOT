4cb1451d2ba5fd32953e5060db401670d91aa25a
takaexe <164596411+takaexe@users.noreply.github.com>
2024-03-24 16:35:41 -0400

    stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,24 +1,9 @@
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
 const db = require("../mongoDB");
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "stop",
-  description: "Stops the music.",
+  description: "Para a música.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -26,7 +11,7 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Nenhuma música tocando!!', ephemeral: true });
       }
 
       queue.stop(interaction.guild.id);
@@ -34,11 +19,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#f1002c')
         .setAuthor({
-          name: 'Music Stopped',
+          name: 'Música parada',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription('**The journey stops, but the rhythm lives on.**')
+        .setDescription('**A viagem para, mas o ritmo continua.**')
         
 
       return interaction.reply({ embeds: [embed] });
