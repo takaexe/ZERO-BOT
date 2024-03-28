e20b9e47d8b862c4ae537982080778827eadfe14
takaexe <164596411+takaexe@users.noreply.github.com>
2024-03-27 20:39:26 -0400

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if(config.shardManager.shardStatus == true){
 
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./bot.js', { token: config.TOKEN || process.env.TOKEN });
-const prefixoComando ='!';
+const prefixCommand ='!';
 const config = require("./config.json");
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
